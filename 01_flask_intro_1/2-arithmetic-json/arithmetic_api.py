5dfa2a975538bf935bb1d38d3c3826c4a9a55a49
Akmal <a.khusanov@gmail.com>
2019-02-10 22:28:56 -0500

    arithmetic api completed

--- 01_flask_intro_1/2-arithmetic-json/arithmetic_api.py
+++ 01_flask_intro_1/2-arithmetic-json/arithmetic_api.py
@@ -4,6 +4,8 @@ app = Flask(__name__)
 
 # example use of request object
 # test this route with localhost:5000/favorites?name=andrew&food=cereal
+
+
 @app.route('/favorites')
 def favorite_things():
     name = request.args.get('name') or 'sally'
@@ -14,6 +16,62 @@ def favorite_things():
         food=food,
         show=show,
     )
+
+
+@app.route('/add')
+def adder():
+    num1 = request.args.get('num1') or '0'
+    num2 = request.args.get('num2') or '0'
+    expression = f"{num1} + {num2}"
+    result = int(num1) + int(num2)
+
+    return jsonify(
+        expression=expression,
+        result=result,
+    )
+
+
+@app.route('/substract')
+def substract():
+    num1 = request.args.get('num1') or '0'
+    num2 = request.args.get('num2') or '0'
+
+    expression = f"{num1} - {num2}"
+    result = int(num1) - int(num2)
+
+    return jsonify(
+        expression=expression,
+        result=result,
+    )
+
+
+@app.route('/multiply')
+def multiply():
+    num1 = request.args.get('num1') or '0'
+    num2 = request.args.get('num2') or '0'
+
+    expression = f"{num1} * {num2}"
+    result = int(num1) * int(num2)
+
+    return jsonify(
+        expression=expression,
+        result=result,
+    )
+
+
+@app.route('/divide')
+def divide():
+    num1 = request.args.get('num1') or '0'
+    num2 = request.args.get('num2') or '1'
+
+    expression = f"{num1} / {num2}"
+    result = int(num1) / int(num2)
+
+    return jsonify(
+        expression=expression,
+        result=result,
+    )
+
 
 if __name__ == "__main__":
     app.run(debug=True)
